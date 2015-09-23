c2d0f8e0d69d8f6290ad36f6da23a21f18230ef6
Iuriydjan <iamvasyur@gmail.com>
2015-09-23 07:44:36 -0700

    adding VolumeMapper and VolumeReducer. Grouping by YearMonthDayHour, and sum traffic Volume through Proxy. Need to consider different strategies to form analytical data. Think to hadoop xml each hour. Then through script or hadoop as well group data by YearMonthDay, so that GUI get tat data as quick as possible

--- src/iam/hadooper/com/DeltaMapper.java
+++ src/iam/hadooper/com/DeltaMapper.java
@@ -11,6 +11,8 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class DeltaMapper extends Mapper<LongWritable, Text,
 IntWritable, IntWritable> {
+	
+	
 
 	
     @Override
@@ -20,22 +22,23 @@ IntWritable, IntWritable> {
     IOException, InterruptedException {
     	int delta =0;
         String document = value.toString();
-        Pattern pacedDurationPattern=Pattern.compile("<E178>(\\d*)</E178>");
-        Matcher pacedDurationMatcher=pacedDurationPattern.matcher(document);
-        
-        Pattern durationPattern=Pattern.compile("<E177>(\\d*)</E177>");
-        Matcher durationMatcher=durationPattern.matcher(document);
-        
-        if  ( pacedDurationMatcher.find() && durationMatcher.find()){
-        	delta = Integer.parseInt(durationMatcher.group(1))-Integer.parseInt(pacedDurationMatcher.group(1));
-        	
-        }else {
-        	delta=-111;
-        }
-        
-        
-        context.write(new IntWritable(delta), new IntWritable(1));
-        }
+
+        	Pattern pacedDurationPattern=Pattern.compile("<E178>(\\d*)</E178>");
+        	Matcher pacedDurationMatcher=pacedDurationPattern.matcher(document);
+            
+            Pattern durationPattern=Pattern.compile("<E177>(\\d*)</E177>");
+            Matcher durationMatcher=durationPattern.matcher(document);
+            
+            if  ( pacedDurationMatcher.find() && durationMatcher.find()){
+            	delta = Integer.parseInt(durationMatcher.group(1))-Integer.parseInt(pacedDurationMatcher.group(1));
+            	
+            }else {
+            	return;
+            }
+            
+            
+            context.write(new IntWritable(delta), new IntWritable(1));
+            }
     }
 
 
